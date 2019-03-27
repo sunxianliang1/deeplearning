fbcb88ccba16f9a7c54c7205cbb31330e29a5fb5
sxl <1243887129@qq.com>
2019-03-27 10:25:24 +0800

    重命名，修改参数

--- TensorFlow/hello/hello/hello/KerasBPminst.py
+++ TensorFlow/hello/hello/hello/KerasBPminst.py
@@ -6,7 +6,7 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dense(200, activation=tf.nn.relu),
   tf.keras.layers.Dropout(0.2),
   tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
@@ -15,4 +15,6 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 model.fit(x_train, y_train, epochs=5)
-model.evaluate(x_test, y_test)
+p=model.evaluate(x_test, y_test)
+print(p)
+#0.9801
