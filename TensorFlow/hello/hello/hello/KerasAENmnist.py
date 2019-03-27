4fc617cb1e98280fd77eb9d30cffe280f5e7d491
sxl <1243887129@qq.com>
2019-03-27 10:43:57 +0800

    keras版mnist自编码网络

---
+++ TensorFlow/hello/hello/hello/KerasAENmnist.py
@@ -0,0 +1,26 @@
+import tensorflow 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.optimizers import SGD
+
+mnist = tensorflow.keras.datasets.mnist
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+x_train.shape=(-1,28,28,1)
+x_test.shape=(-1,28,28,1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
