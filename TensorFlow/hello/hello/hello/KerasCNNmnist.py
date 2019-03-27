4ebf57b9b46c77c446e8c0e33370f3c7362e2056
sxl <1243887129@qq.com>
2019-03-27 10:27:03 +0800

    keras版本mnist的cnn网络

---
+++ TensorFlow/hello/hello/hello/KerasCNNmnist.py
@@ -0,0 +1,38 @@
+﻿import tensorflow 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.optimizers import SGD
+
+mnist = tensorflow.keras.datasets.mnist
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+x_train.shape=(-1,28,28,1)
+x_test.shape=(-1,28,28,1)
+print(x_train.shape)
+
+model = Sequential()
+#model.add(Flatten(input_shape=(28, 28)))
+model.add(Conv2D(32, (5, 5), activation='relu', input_shape=(28, 28,1)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(200, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+p=model.evaluate(x_test, y_test)
+print(p)
+#0.993
