7927fddc3873bbadb519658274a23b345ffda202
sxl <1243887129@qq.com>
2019-03-28 20:08:09 +0800

    cnn版本基于自编码的mnist

---
+++ TensorFlow/hello/hello/hello/KerasAENmnist1.py
@@ -0,0 +1,72 @@
+import tensorflow 
+from tensorflow.keras.models import Sequential,Model
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D,UpSampling2D
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras import Input
+
+mnist = tensorflow.keras.datasets.mnist
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+x_train.shape=(-1,784)
+x_test.shape=(-1,784)
+
+encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+
+# this is our input placeholder
+input_img = Input(shape=(784,))
+# "encoded" is the encoded representation of the input
+encoded = Dense(encoding_dim, activation='relu')(input_img)
+# "decoded" is the lossy reconstruction of the input
+decoded = Dense(784, activation='relu')(encoded)
+
+
+# this model maps an input to its reconstruction
+autoencoder = Model(inputs=input_img, outputs=decoded)
+autoencoder.compile(optimizer='adadelta', loss='mean_squared_error')
+autoencoder.fit(x_train, x_train,
+                nb_epoch=5,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+p=autoencoder.evaluate(x_test, x_test)
+print(p)
+
+
+# this model maps an input to its encoded representation
+encoder = Model(inputs=input_img, outputs=encoded)
+# create a placeholder for an encoded (32-dimensional) input
+encoded_input = Input(shape=(encoding_dim,))
+# retrieve the last layer of the autoencoder model
+decoder_layer = autoencoder.layers[-1]
+# create the decoder model
+decoder = Model(inputs=encoded_input, outputs=decoder_layer(encoded_input))
+
+
+
+import matplotlib.pyplot as plt
+
+encoded_imgs = encoder.predict(x_test)
+decoded_imgs = decoder.predict(encoded_imgs)
+
+
+n = 10  # how many digits we will display
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # display original
+    ax = plt.subplot(2, n, i+1)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i + n+1)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
+
+
