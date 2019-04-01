3ef9ae99740ff40651b737be7b2d4482067b8da8
sxl <1243887129@qq.com>
2019-04-01 15:16:09 +0800

    ECT相关算法

---
+++ ECT/ECT/ECT.py
@@ -0,0 +1,8 @@
+﻿from scipy.io import loadmat
+
+data = loadmat('E:\deeplearning\ECT\数据生成\data\data1')
+
+
+
+
+
---
+++ ECT/ECT/loaddata.py
@@ -0,0 +1,11 @@
+from scipy.io import loadmat
+class ECTdata:
+    def ECTdata(self,path):
+
+       
+        data = loadmat('E:\deeplearning\ECT\数据生成\data\data1')
+
+
+
+
+
