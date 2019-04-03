feae91b07a412ecc4bac6e8fd53cfd5bed3d4dc6
sxl <1243887129@qq.com>
2019-04-03 17:27:04 +0800

    数据载入模块和绘图模块测试代码，已测试完成

---
+++ ECT/ECT/draw.py
@@ -0,0 +1,13 @@
+﻿from loaddata   import ECTdata
+import matplotlib.pyplot as mp
+import numpy as np
+
+mydata=ECTdata('E:\deeplearning\ECT\数据生成\data')
+mydata.initsca()
+a=mydata.index.index(2)
+mydata.drawsca(mydata.images[a])
+print(mydata.images[a].sum())
+mp.show()
+
+
+
