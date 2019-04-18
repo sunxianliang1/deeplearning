d4e69e175d0479d7e2a8ca712322e3fc3d5bb4f4
sxl <1243887129@qq.com>
2019-04-18 10:14:01 +0800

    原件

--- ECT/ECT/ECT.py
+++ ECT/ECT/ECT.py
@@ -1,6 +1,31 @@
-﻿from scipy.io import loadmat
+﻿from loaddata   import ECTdata
+import tensorflow 
+from tensorflow.keras.models import Sequential,Model
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras import Input
 
-data = loadmat('E:\deeplearning\ECT\数据生成\data\data1')
+
+
+
+mydata=ECTdata('E:\deeplearning\ECT\数据生成\data')
+
+input=Input(shape=(mydata.imgsize,))
+encoded = Dense(140, activation='relu')(input)
+mid = Dense(mydata.capsize, activation='relu')(encoded)
+decoded = Dense(140, activation='relu')(mid)
+output=Dense(mydata.imgsize, activation='relu')(decoded)
+
+model=Model(inputs=input,outputs=output)
+model.compile(optimizer='adadelta', loss='mean_squared_error')
+model.fit(mydata.imgtrain,mydata.imgtrain,epochs=100,shuffle=True,validation_data=(mydata.imgtest,mydata.imgtest))
+p=model.evaluate(mydata.imgtest,mydata.imgtest)
+print(p)
+
+
+
+
+
+
 
 
 
